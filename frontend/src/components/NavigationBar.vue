f913650e78248ca590dc8717a66a677189cab7bd
Chokchai213 <77828500+Chokchai213@users.noreply.github.com>
2024-05-08 02:16:34 +0700

    Edit Footer, Edit NavigationBar

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -1,17 +1,15 @@
 <script setup></script>
 <template>
   <nav class="bg-green-700">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div class="relative flex h-16 items-center justify-between">
-        <div
-          class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
-        >
+    <div class="mx-auto">
+      <div class="relative flex items-center justify-between px-4">
+        <div class="flex items-center">
           <div class="flex flex-shrink-0 items-center">
-            <a href="#">
-              <h1 class="font-bold select-none">FUNDMAIJA</h1>
+            <a href="#" class="px-4 py-4">
+              <h1 class="font-bold select-none text-2xl">FUNDMAIJA</h1>
             </a>
           </div>
-          <div class="hidden sm:ml-6 sm:block">
+          <div class="sm:ml-6 sm:block">
             <div class="flex space-x-4">
               <a
                 href="#"
@@ -26,16 +24,19 @@
             </div>
           </div>
         </div>
-        <div
-          class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
-        >
-          <div class="relative ml-3">
-            <div>
-              <img
-                class="h-8 w-8 rounded-full border border-black border-2"
-                src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
-                alt=""
-              />
+        <div class="relative flex items-center">
+          <div class="mr-3">
+            <img
+              class="h-8 w-8 rounded-full border border-black border-2"
+              src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <div
+              class="bg-gray-900 text-white rounded-3xl px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+            >
+              UserName
             </div>
           </div>
         </div>
