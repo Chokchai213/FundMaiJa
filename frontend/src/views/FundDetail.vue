352ae72966c6e3e12860905ecadc75807fcd7ace
ThunyavutN <thunyavut.n@gmail.com>
2024-05-09 23:30:14 +0700

    Add Go back button

--- frontend/src/views/FundDetail.vue
+++ frontend/src/views/FundDetail.vue
@@ -2,32 +2,36 @@
 import OverlayLoading from "../components/OverlayLoading.vue";
 </script>
 <template>
-    <div class="flex justify-center mt-16 mb-16">
-        <div class="flex flex-col items-center w-2/4">
+    <div class="flex justify-center mt-14 mb-12">
+        <div class="bg-gray-50 p-8 rounded-lg shadow-md w-3/4 items-center justify-center flex flex-col">
             <div class="flex flex-col px-4 sm:px-0 items-center ">
-                <h3 class="text-base font-semibold leading-7 text-gray-900">{{ Detail.proj_name_en }}</h3>
+                <h3 class="text-base font-semibold leading-7 text-gray-900 text-xl">{{ Detail.proj_name_en }}</h3>
                 <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">{{ Detail.proj_name_th }}</p>
             </div>
             <div class="mt-6 border-t border-gray-100">
                 <dl class="divide-y divide-gray-100">
                     <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                         <dt class="text-sm font-medium leading-6 text-gray-900">Abbreviated name</dt>
-                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 ">{{ Detail.proj_abbr_name
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 ">{{
+                            Detail.proj_abbr_name
                             }}</dd>
                     </div>
                     <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                         <dt class="text-sm font-medium leading-6 text-gray-900">Risk</dt>
-                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ Detail.risk_spectrum
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
+                            Detail.risk_spectrum
                             }}</dd>
                     </div>
                     <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                         <dt class="text-sm font-medium leading-6 text-gray-900">Type</dt>
-                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ Detail.spec_code }}
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ Detail.spec_code
+                            }}
                         </dd>
                     </div>
                     <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                         <dt class="text-sm font-medium leading-6 text-gray-900">Policy description</dt>
-                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ Detail.policy_desc }}
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ Detail.policy_desc
+                            }}
                         </dd>
                     </div>
                     <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -41,23 +45,22 @@ import OverlayLoading from "../components/OverlayLoading.vue";
                         <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
                             Detail.investment_policy_desc }}</dd>
                     </div>
-                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 items-center">
                         <dt class="text-sm font-medium leading-6 text-gray-900">Factsheet</dt>
                         <dd class="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                            <ul role="list" class="divide-y divide-gray-100 rounded-md border border-gray-200">
-                                <li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                                    <div class="ml-4 flex-shrink-0">
-                                        <a :href="Detail.url_factsheet"
-                                            class="font-medium text-indigo-600 hover:text-indigo-500">{{
-                                                Detail.url_factsheet }}</a>
-                                    </div>
-                                </li>
-                            </ul>
+                            <a :href="Detail.url_factsheet" class="font-medium text-indigo-600 hover:text-indigo-500">{{
+                                Detail.url_factsheet }}</a>
                         </dd>
                     </div>
                 </dl>
             </div>
         </div>
+    </div>
+    <div class="flex justify-center mb-20">
+        <button @click="$router.back()"
+            class="bg-gray-50 text-black rounded-xl shadow-md px-12 py-5 text-md hover:bg-gray-100 font-medium cursor-pointer">
+            Go back
+        </button>
     </div>
     <OverlayLoading :isLoading="isLoading" />
 </template>
