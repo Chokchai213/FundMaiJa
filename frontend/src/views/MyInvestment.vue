2fa560cd77c7f2c6d6a27b21c349947751b6ce05
Chokchai213 <77828500+Chokchai213@users.noreply.github.com>
2024-05-09 08:07:41 +0700

    Create MyInvestment.vue

---
+++ frontend/src/views/MyInvestment.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { getCookie } from '../utils/CookieUtils';
+
+</script>
+
+
+<template>
+  <p>MyInvestment</p>
+</template>
+  
+  <script>
+  import { getCookie } from '../utils/CookieUtils';
+    export default {
+  mounted(){
+      console.log(getCookie("accessToken"));
+    },
+  }
+  </script>
+  
+  <style>
+  </style>
